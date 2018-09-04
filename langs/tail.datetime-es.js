d6123c69a340c35244227769836d17373a6b638a
SamBrishes <sambrishes@gmx.net>
2018-09-04 18:38:27 +0200

    Add Spanish

---
+++ langs/tail.datetime-es.js
@@ -0,0 +1,9 @@
+;(function(w){
+    w.tail.DateTime.strings = {
+        months: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+        days:   ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"],
+        shorts: ["DOM", "LUN", "MAR", "MIÉ", "JUE", "VIE", "SÁB"],
+        time:   ["Horas", "Minutos", "Segundos"],
+        header: ["Selecciona un mes", "Seleccione un año", "Seleccione una hora"],
+    };
+})(window);
